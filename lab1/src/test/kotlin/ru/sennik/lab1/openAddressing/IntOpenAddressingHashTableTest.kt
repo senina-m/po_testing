5a32f63f573c5ca90e39e81dace4432ea730b4ba
nanikon <nata.nik.18@mail.ru>
2023-02-20 02:48:30 +0300

    open addressing: fix test

--- lab1/src/test/kotlin/ru/sennik/lab1/openAddressing/IntOpenAddressingHashTableTest.kt
+++ lab1/src/test/kotlin/ru/sennik/lab1/openAddressing/IntOpenAddressingHashTableTest.kt
@@ -10,24 +10,6 @@ import org.junit.jupiter.params.provider.ValueSource
  */
 class IntOpenAddressingHashTableTest {
     @Test
-    fun tryInvokeFunctions() {
-        val map  = IntOpenAddressingHashTableImpl<String>(75, 4)
-
-        map.put(3, "3")
-        map.put(7, "7")
-        Assertions.assertIterableEquals(
-            listOf(Pair(7, "7"), null, null, Pair(3, "3")),
-            map.getList()
-        )
-
-        Assertions.assertThrows(NotFoundKeyException::class.java) { map.get(1) }
-        Assertions.assertThrows(NotFoundKeyException::class.java) { map.delete(1) }
-
-        Assertions.assertEquals("3", map.get(3))
-        map.delete(3)
-    }
-
-    @Test
     fun `Create table with negative initialSize failed`() {
         Assertions.assertThrows(
             IllegalArgumentException::class.java,
