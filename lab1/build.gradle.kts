d69fdabbd0187e02433a6ed1e0e2b343269b8b0c
nanikon <nata.nik.18@mail.ru>
2023-02-19 18:32:58 +0300

    cos: implement test

--- lab1/build.gradle.kts
+++ lab1/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.0.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
