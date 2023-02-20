a6cabd3c597dca086c3774709eb7533df3b8b8b4
marsen <senina.m.m@yandex.ru>
2023-02-20 14:24:31 +0300

    setup jacoco testing

--- lab1/build.gradle.kts
+++ lab1/build.gradle.kts
@@ -1,9 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.7.22"
-    java
-}
+        plugins {
+            kotlin("jvm") version "1.7.22"
+            java
+            pmd
+            jacoco
+        }
 
 group = "ru.sennik"
 version = "0.0.1-SNAPSHOT"
@@ -17,8 +19,8 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.0.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation ("org.junit.jupiter:junit-jupiter-params:5.8.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -28,6 +30,60 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-tasks.withType<Test> {
+tasks.test {
     useJUnitPlatform()
+    dependsOn(tasks.pmdMain)
+    dependsOn(tasks.pmdTest)
+    finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+}
+
+pmd {
+    isConsoleOutput = true
+    toolVersion = "6.21.0"
+    ruleSetFiles = files("config/pmd.xml") // Исключения только через внешний файл
+    ruleSets = mutableListOf<String>()
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                counter = "CLASS"
+                value = "MISSEDCOUNT"
+                minimum = BigDecimal(0)
+            }
+        }
+        rule {
+            limit {
+                counter = "METHOD"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal(0.9)
+            }
+        }
+        rule {
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal(0.9)
+            }
+        }
+        rule {
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal(0.9)
+            }
+        }
+    }
+}
+tasks.check {
+    dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
