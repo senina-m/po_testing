b239633e81d5c30e67ef5f67ca4eb109f1d912b6
nanikon <nata.nik.18@mail.ru>
2023-02-20 03:30:35 +0300

    cos: cosmetic fix

--- lab1/src/main/kotlin/ru/sennik/lab1/math/Cos.kt
+++ lab1/src/main/kotlin/ru/sennik/lab1/math/Cos.kt
@@ -1,42 +1,37 @@
 package ru.sennik.lab1.math
-import java.lang.Math.PI
-import java.lang.RuntimeException
+
 import java.math.BigDecimal
 
 fun cos(x: Double, accuracy: Double): Double {
-    if(accuracy >= 1 || accuracy <= 0) {
+    if (accuracy >= 1 || accuracy <= 0) {
         throw RuntimeException("Accuracy has to be from 0 to 1!")
     }
     val xb = BigDecimal(x)
-    val acc = BigDecimal(accuracy*0.1)
-    var cosX = BigDecimal(1.0)
-    var f =  BigDecimal(1)
-    var crx = BigDecimal(1.0)
-    var n = BigDecimal(1)
+    val xb2 = xb * xb
+    val acc = BigDecimal(accuracy * 0.1)
+
+    var cosX = BigDecimal.ONE
+    var f =  BigDecimal.ONE
+    var crx = BigDecimal.ONE
+    var n = BigDecimal.ONE
     var i = 1
-    while((crx / f) >= acc) {
-        if (i  > 20) {
-            break
-        }
-        f *= n*(n + BigDecimal(1))
-        crx *= xb*xb
+
+    var summand = crx / f
+    while (summand >= acc) {
+        if (i  > 20) break
+
+        f *= n * (n + BigDecimal.ONE)
+        crx *= xb2
+        summand = crx / f
+
         n += BigDecimal(2)
         i += 1
-        if (i % 2 == 0){
-            cosX -= crx/f
-            print("${-crx/f}\n")
-
-        }else{
-            cosX += crx/f
-            print("${crx/f}\n")
+        if (i % 2 == 0) {
+            cosX -= summand
+        } else {
+            cosX += summand
         }
-        print("$cosX\n")
     }
 
     return cosX.toDouble()
-}
-
-fun main(){
-    print("result ${cos(2.0, 0.001)} \n")
-    print("cos act = ${kotlin.math.cos(2.0)}")
 }
