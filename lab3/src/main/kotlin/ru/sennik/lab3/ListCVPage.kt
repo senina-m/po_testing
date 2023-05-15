c5ca3f8ba809793c2cc8eddca394814ddb19ecba
nanikon <nata.nik.18@mail.ru>
2023-05-15 03:06:51 +0300

    lab3: add delete cv to end of test

--- lab3/src/main/kotlin/ru/sennik/lab3/ListCVPage.kt
+++ lab3/src/main/kotlin/ru/sennik/lab3/ListCVPage.kt
@@ -1,5 +1,6 @@
 package ru.sennik.lab3
 
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
@@ -21,5 +22,14 @@ class ListCVPage(driver: WebDriver) {
 
    fun clickCreateCVButton(){
       createCVButton!!.click()
+   }
+
+   fun openCVByName(name: String) {
+      var i = 0
+      do {
+         i++
+         val currentName = driver.findElement(By.xpath("//*[@id=\"HH-React-Root\"]/div/div[3]/div[1]/div/div/div[1]/div[5]/div[$i]/div/h3/a/span"))
+      } while (currentName.text != name)
+      driver.findElement(By.xpath("//*[@id=\"HH-React-Root\"]/div/div[3]/div[1]/div/div/div[1]/div[5]/div[$i]/div/h3/a")).click()
    }
 }
